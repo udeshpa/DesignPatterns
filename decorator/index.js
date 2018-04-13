081ec6d0208f7ef04449319772b5521181ff4f64
Uday Deshpande <udaydeshpande@Udays-MacBook-Pro.local>
2018-04-13 16:43:22 -0500

    multiple patterns

---
+++ decorator/index.js
@@ -0,0 +1,21 @@
+var Shopper = require('./Shopper');
+var {
+  InventoryItem,
+  GoldenInventoryItem,
+  DiamondInventoryItem
+} = require('./InventoryItem.js');
+
+var alex = new Shopper('Alex', 3000);
+
+var walkman = new InventoryItem("Walkman", 29.99);
+var necklace = new InventoryItem("Necklace", 9.99);
+
+var gold_necklace = new GoldenInventoryItem(necklace);
+var diamond_gold_necklace = new DiamondInventoryItem(gold_necklace);
+
+var diamond_walkman = new DiamondInventoryItem(walkman);
+
+alex.purchase(gold_necklace);
+alex.purchase(diamond_walkman);
+
+alex.printStatus();
